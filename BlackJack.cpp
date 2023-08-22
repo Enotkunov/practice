db789a06bb460b5143a29684e778c3db243c52b4
Enotkunov <132558966+Enotkunov@users.noreply.github.com>
2023-08-22 17:13:20 +0300

    Update BlackJack.cpp

--- BlackJack.cpp
+++ BlackJack.cpp
@@ -301,10 +301,12 @@ void startGame()
 }
 
 
-void exercise4()
+int main()
 {
 	srand(static_cast<unsigned int>(time(0))); // используем системные часы в качестве стартового значения
 	rand(); // пользователям Visual Studio: делаем сброс первого случайного числа
 
 	startGame();
+
+	return 0;
 }
